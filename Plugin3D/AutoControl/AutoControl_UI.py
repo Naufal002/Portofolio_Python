e774262c1331b5803ac8c6e647259bc3112dedb7
Naufal002 <80965666+Naufal002@users.noreply.github.com>
2022-07-14 11:20:55 +0700

    new problem /39

--- Plugin3D/AutoControl/AutoControl_UI.py
+++ Plugin3D/AutoControl/AutoControl_UI.py
@@ -27,7 +27,7 @@ class TransferVertex:
         cmds.separator( style="none", height=5 )
         cmds.text( label="Add Controll" )
         cmds.separator( style="none", height=3 )
-        cmds.button( label="Finall Button", c = self.finall_fnc)
+        cmds.button( label="Finall Button",c = self.finall_fnc)
         
         cmds.showWindow()
 
@@ -36,12 +36,39 @@ class TransferVertex:
         '''SEARCH FUNCTION'''
         sel = cmds.ls(sl = True, type = "joint")
         for i in sel:
-            pass
+            if
+
+
+            
             
     
 
-    def finall_fnc(self):
+    def finall_fnc(self,*args):
         '''FINALL BUTTON'''
-        pass
-            
+        sel = cmds.ls(sl = True, type = "joint")
+        name_controls = "_CONTROL"
+        for i in sel:
+
+            qry_translate = cmds.xform( i, query=True, ws=True, t=True )
+            qry_rotate = cmds.xform( i, query=True, ws=True, ro=True )
+
+            control = cmds.circle(name = name_controls.format(i))
+            grp = cmds.group(name = "Main Group".format(i))
+
+            cmds.xform( grp, ws=True, t=qry_translate, ro=qry_rotate )
+            cmds.parent(control, i, grp)
+
 TransferVertex().TransferVertex_UI()
+
+
+
+'''
+ for i in sel:
+            x = i.split(".")[-1]
+            var_text = cmds.textField( "Search_TF", q=True, text=True ) 
+            
+            if var_text == "" :
+                cmds.textField( "Search_TF", e=True, text=x )
+            else :
+                cmds.textField( "Search_TF", e=True, text=var_text + ", " + x )
+'''
